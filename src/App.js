302f0d69d461a63a160113c92a233ef8d91e117f
Akhila Muthyala <akhilamuthyala@Mac.attlocal.net>
2025-03-09 19:18:24 -0500

    updated sidebar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Intro from "./components/Intro";
 import About from "./components/About";
+import Sidebar from "./components/Sidebar";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
@@ -11,6 +12,7 @@ const App = () => {
   return (
     <div>
       <Header />  
+      <Sidebar />
       <Intro />
       <About />
       <Experience />
---
+++ src/components/Sidebar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../styles/Sidebar.css";
+import { FaGithub, FaInstagram, FaLinkedin, FaEnvelope } from "react-icons/fa"; // React Icons
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <a href="https://github.com/amuthyal" target="_blank" rel="noopener noreferrer">
+        <FaGithub className="sidebar-icon" />
+      </a>
+      <a href="https://www.instagram.com/akreddy07/" target="_blank" rel="noopener noreferrer">
+        <FaInstagram className="sidebar-icon" />
+      </a>
+      <a href="https://www.linkedin.com/in/akhila-muthyala-48b776209/" target="_blank" rel="noopener noreferrer">
+        <FaLinkedin className="sidebar-icon" />
+      </a>
+      <a href="mailto:muthyala.akhila07@gmail.com" target="_blank" rel="noopener noreferrer">
+        <FaEnvelope className="sidebar-icon" />
+      </a>
+    </div>
+  );
+};
+
+export default Sidebar;
